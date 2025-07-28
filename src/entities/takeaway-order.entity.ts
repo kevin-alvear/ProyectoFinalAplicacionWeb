d6ffaff7dcf38d3489ac32b43e02c994b2776d84
KEVIN XAVIER ALVEAR CACHIPUENDO <kevin.alvear@epn.edu.ec>
2025-07-28 00:04:51 -0500

    Delete src directory

--- src/entities/takeaway-order.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ChildEntity } from 'typeorm';
-import { AbstractOrder } from './order.entity';
-
-@ChildEntity()
-export class TakeAwayOrder extends AbstractOrder {
-  calculateTotalPayment() {
-    this.totalPayment = this.menus.reduce((acc, menu) => acc + menu.price, 0);
-  }
-}
